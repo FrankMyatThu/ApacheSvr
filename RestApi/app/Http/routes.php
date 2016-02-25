2a10ee12001846d47a99c4ad5cd387cbe92ca6ec
frank.myatthu <myatthu1986@gmail.com>
2016-02-25 21:27:03 +0800

    modify get

--- RestApi/app/Http/routes.php
+++ RestApi/app/Http/routes.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
 
 
 Route::get('/api/v1/employees/{id?}', 'Employees@index');
-Route::post('/api/v1/employees', 'Employees@store');
+Route::get('/api/v1/employees', 'Employees@store');
 Route::post('/api/v1/employees/{id}', 'Employees@update');
 Route::delete('/api/v1/employees/{id}', 'Employees@destroy');
