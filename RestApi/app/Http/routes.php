40d0fc260d8cf8e4b8bbfe1cd773d0ffba3b2397
myat <myat@medialink.com.sg>
2016-03-07 17:37:47 +0800

    hello world controller

--- RestApi/app/Http/routes.php
+++ RestApi/app/Http/routes.php
@@ -2,21 +2,33 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Routes File
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
+| Here is where you will register all of the routes in an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
 
+Route::get('hello', 'Hello@index');
+Route::get('hello/{name}', 'Hello@show');
 
-Route::get('/api/v1/employees/{id?}', 'Employees@index');
-Route::post('/api/v1/employees', 'Employees@store');
-Route::post('/api/v1/employees/{id}', 'Employees@update');
-Route::delete('/api/v1/employees/{id}', 'Employees@destroy');
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+});
