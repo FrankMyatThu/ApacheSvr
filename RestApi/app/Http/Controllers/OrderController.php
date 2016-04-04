b8836bcbbbe19ad16b3686021bf0036ea48d54cf
myat <myat@medialink.com.sg>
2016-04-04 18:48:27 +0800

    OrderController

--- RestApi/app/Http/Controllers/OrderController.php
+++ RestApi/app/Http/Controllers/OrderController.php
@@ -10,9 +10,86 @@ use Log;
 
 class OrderController extends Controller
 {
-    public function SelectOrderAll()
+	// Create	
+    public function CreateOrder(Request $request)
 	{
-		Log::info('[OrderController][SelectOrderAll()]');
-		return "SelectOrderAll ...";
+		Log::info("[OrderController/CreateOrder]");
+		$requestContent = json_decode($request->getContent(), true);
+		$validator = OrderViewModel_Binding::validate($requestContent[0]);	
+		
+		if($validator->fails()){			
+			Log::info("[OrderController/CreateOrder] validator fails message = ".$validator->messages()) ;
+			return $validator->messages();
+		}else{
+			$OrderViewModel_Binding = new OrderViewModel_Binding();
+			$OrderViewModel_Binding->fill($requestContent[0]);
+			return (new Order_BL())->CreateOrder($OrderViewModel_Binding);			
+		}
+	}
+
+	// Retrieve
+	public function SelectOrderWithoutPager(Request $request)
+	{
+		Log::info("[OrderController/SelectOrderWithoutPager]");
+		$requestContent = json_decode($request->getContent(), true);
+		$validator = OrderViewModel_Criteria::validate($requestContent[0]);	
+
+		if($validator->fails()){			
+			Log::info("[OrderController/SelectOrderWithoutPager] validator fails message = ".$validator->messages()) ;
+			return $validator->messages();
+		}else{
+			$OrderViewModel_Criteria = new OrderViewModel_Criteria();
+			$OrderViewModel_Criteria->fill($requestContent[0]);
+			return (new Order_BL())->SelectOrderWithoutPager($OrderViewModel_Criteria);			
+		}
+	}
+	public function SelectOrderWithPager(Request $request)
+	{
+		Log::info("[OrderController/SelectOrderWithPager]");		
+		$requestContent = json_decode($request->getContent(), true);		
+		$validator = OrderViewModel_Criteria::validate($requestContent[0]);	
+
+		if($validator->fails()){			
+			Log::info("[OrderController/SelectOrderWithPager] validator fails message = ".$validator->messages()) ;
+			return $validator->messages();
+		}else{
+			$OrderViewModel_Criteria = new OrderViewModel_Criteria();			
+			$OrderViewModel_Criteria->fill($requestContent[0]);
+			return (new Order_BL())->SelectOrderWithPager($OrderViewModel_Criteria);			
+		}
+	}
+	
+	// Update
+	public function UpdateOrder(Request $request)
+	{
+		Log::info("[OrderController/UpdateOrder]");
+		$requestContent = json_decode($request->getContent(), true);
+		$validator = OrderViewModel_Binding::validate($requestContent[0]);	
+		
+		if($validator->fails()){			
+			Log::info("[OrderController/UpdateOrder] validator fails message = ".$validator->messages()) ;
+			return $validator->messages();
+		}else{
+			$OrderViewModel_Binding = new OrderViewModel_Binding();
+			$OrderViewModel_Binding->fill($requestContent[0]);
+			return (new Order_BL())->UpdateOrder($OrderViewModel_Binding);			
+		}
+	}
+
+	// Delete
+	public function DeleteOrder(Request $request)
+	{
+		Log::info("[OrderController/DeleteOrder]");
+		$requestContent = json_decode($request->getContent(), true);
+		$validator = OrderViewModel_Binding::validate($requestContent[0]);	
+		
+		if($validator->fails()){			
+			Log::info("[OrderController/DeleteOrder] validator fails message = ".$validator->messages()) ;
+			return $validator->messages();
+		}else{
+			$OrderViewModel_Binding = new OrderViewModel_Binding();
+			$OrderViewModel_Binding->fill($requestContent[0]);
+			return (new Order_BL())->DeleteOrder($OrderViewModel_Binding);			
+		}
 	}
 }
