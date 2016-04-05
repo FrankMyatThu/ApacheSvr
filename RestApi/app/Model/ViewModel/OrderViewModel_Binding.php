6686833a3f5e5f25cf937d39959286065c30bcc3
myat <myat@medialink.com.sg>
2016-04-05 18:43:41 +0800

    optimized regex

---
+++ RestApi/app/Model/ViewModel/OrderViewModel_Binding.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Model\ViewModel;
+
+use Illuminate\Support\Facades\Config;
+use Illuminate\Database\Eloquent\Model;
+use Log;
+
+// To bind data
+class OrderViewModel_Binding extends Model
+{
+	protected $fillable = array(		
+						 	'SrNo',					 	
+						 	'OrderId',
+						 	'Description',
+						 	'OrderDate'
+						   );
+	
+	protected function validate($data){	
+		$validator = "";	
+		$rules = array(
+					'SrNo'     				=> ['sometimes', Config::get('FormatStandardizer.SrNo') ],					
+					'ProductID' 			=> ['sometimes', Config::get('FormatStandardizer.ProductID') ],
+					'ProductName'  			=> ['required', Config::get('FormatStandardizer.ProductName') , 'min:1', 'max:250'],					
+					'Description'  			=> ['required', Config::get('FormatStandardizer.ProductDescription') , 'min:1', 'max:250'],
+					'Price'  				=> ['required', Config::get('FormatStandardizer.Price') ],
+					//'ProductImage'			=> ['sometimes', Config::get('FormatStandardizer.ImagePath') ]
+				);	
+		$messages = array(
+					'SrNo.regex' 				=> 'Invalid SrNo.',					
+					'ProductID.regex' 			=> 'Invalid ProductID.',
+					'ProductName.required' 		=> 'Product Name is required.',
+					'ProductName.regex'			=> 'Invalid Product Name.',
+					'ProductName.min'			=> 'Product Name\'s length should be between 1 and 250.',
+					'ProductName.max'			=> 'Product Name\'s length should be between 1 and 250.',
+					'Description.required' 		=> 'Description is required.',
+					'Description.regex'			=> 'Invalid Description.',
+					'Description.min'			=> 'Description\'s length should be between 1 and 250.',
+					'Description.max'			=> 'Description\'s length should be between 1 and 250.',
+					'Price.required'			=> 'Price is required',
+					'Price.regex'   			=> 'Invalid Price',
+					//'ProductImage.regex'		=> 'Invalid ProductImage',
+				);
+				
+		Log::info('[OrderViewModel_Binding/validate] validate function start');				
+		//Log::info('[OrderViewModel_Binding/validate] $data'.PHP_EOL. print_r($data, true));
+
+		$validator = \Validator::make($data, $rules, $messages);				
+		return $validator;
+	}
+}
--- RestApi/config/FormatStandardizer.php
+++ RestApi/config/FormatStandardizer.php
@@ -4,9 +4,12 @@ return  [
  
  	'OrderByClause'				=> 'regex:/^([A-Za-z0-9_]+\s(ASC|DESC),?\s?){1,}$/',
  	'SrNo' 						=> 'regex:/^[0-9.]*$/', 	
+ 	'ID'						=> 'regex:/^[0-9.]*$/',
+ 	'Name'						=> 'regex:/^[A-Za-z0-9.\s]+$/',
+
     'ProductID' 				=> 'regex:/^[0-9.]*$/',
-    'ProductName' 				=> 'regex:/^[A-Za-z0-9 ,.\'\"\-\(\)\/]+$/',
-    'ProductDescription' 		=> 'regex:/^[A-Za-z0-9 ,.\'\"\-\(\)\/]+$/',    
+    'ProductName' 				=> 'regex:/^[A-Za-z0-9.\s]+$/',
+    'ProductDescription' 		=> 'regex:/^[A-Za-z0-9.\s]+$/',    
     'Price'						=> 'regex:/^[0-9.]*$/',    
     'ImagePath'					=> 'regex:/^.*$/'
 
